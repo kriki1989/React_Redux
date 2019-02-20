22e28e540a217809ad18745b0f2be26db0c65963
Maria Karyda <mariakarida@gmail.com>
2019-02-20 18:03:58 +0200

    Switch statement

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -5,14 +5,14 @@ import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    render () {
+    render() {
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
-                <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
-                <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter}  />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
+                <CounterControl label="Add 5" clicked={this.props.onAddCounter} />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter} />
             </div>
         );
     }
@@ -26,10 +26,10 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
-        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
-        onAddCounter: () => dispatch({type: 'ADD', value: 5}),
-        onSubtractCounter: () => dispatch({type: 'SUBTRACT', value: 5})
+        onIncrementCounter: () => dispatch({ type: 'INCREMENT' }),
+        onDecrementCounter: () => dispatch({ type: 'DECREMENT' }),
+        onAddCounter: () => dispatch({ type: 'ADD', value: 5 }),
+        onSubtractCounter: () => dispatch({ type: 'SUBTRACT', value: 5 })
     };
 };
 
