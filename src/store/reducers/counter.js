a33901d2d5c8d3d490aabf92f20832c9cc1e5883
Maria Karyda <mariakarida@gmail.com>
2019-02-22 12:30:25 +0200

    Leaner code in reducers

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     counter: 0
@@ -6,25 +7,10 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.INCREMENT:
-            const newState = Object.assign({}, state);
-            newState.counter = state.counter + 1;
-            return newState;
-        case actionTypes.DECREMENT:
-            return {
-                ...state,
-                counter: state.counter - 1
-            }
-        case actionTypes.ADD:
-            return {
-                ...state,
-                counter: state.counter + action.value
-            }
-        case actionTypes.SUBTRACT:
-            return {
-                ...state,
-                counter: state.counter - action.value
-            }
+        case actionTypes.INCREMENT : return updateObject( state, {counter: state.counter + 1});
+        case actionTypes.DECREMENT : return updateObject( state, {counter: state.counter - 1});
+        case actionTypes.ADD : return updateObject( state, {counter: state.counter + action.value});
+        case actionTypes.SUBTRACT : return updateObject( state, {counter: state.counter - action.value});
     }
     return state;
 };
